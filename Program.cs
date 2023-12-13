7a1f0e8dc3a73013559f1d63335cd4ed418902ca
Anderson Oliveira <anderson.l.o@outlook.com>
2023-12-13 20:34:05 -0300

    Criando o Projeto Explorando

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.NetworkInformation;
+using ExemploExplorando.Models;
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Anderson";
+// p1.Sobrenome = "Oliveira";
+// p1.Idade = 33;
+// p1.Apresentar();
+
+Pessoa p1 = new Pessoa(nome:"Anderson", sobrenome:"Oliveira");
+Pessoa p2 = new Pessoa(nome:"Luiz", sobrenome:"Oliveira");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
