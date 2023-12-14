443b03e59488a3fa1539aa0eb251fa76c5d12cc2
Anderson Oliveira <anderson.l.o@outlook.com>
2023-12-14 19:58:38 -0300

    Mnipulando Valores com C#

--- Program.cs
+++ Program.cs
@@ -1,5 +1,53 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Globalization;
+using System.Net.NetworkInformation;
 using ExemploExplorando.Models;
+
+
+
+
+
+
+
+
+
+// string dataString = "2023-04-32 18:00";
+
+// bool sucesso = DateTime.TryParseExact(dataString, 
+//                         "yyyy-MM-dd HH:mm", 
+//                         CultureInfo.InvariantCulture, 
+//                         DateTimeStyles.None, out DateTime data);
+// if(sucesso)
+// {
+//     Console.WriteLine($"Conversão com sucesso! Data: {data}");
+    
+// }
+// else
+// {
+//     Console.WriteLine($"{dataString} não é uma data válida");
+// }
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 1582.40M;
+
+// Console.WriteLine(valorMonetario.ToString("C8"));
+
+// double porcetagem = .3421;
+
+// Console.WriteLine(porcetagem.ToString("P"));
+
+// int numero = 123456;
+
+// Console.WriteLine(numero.ToString("##-##-##"));
+
+
+// string numero1 =   "10";
+// string numeor2 = "20";
+
+// string resultado = numero1 + numeor2;
+
+// Console.WriteLine(resultado);
 
 // Pessoa p1 = new Pessoa();
 // p1.Nome = "Anderson";
@@ -7,13 +55,13 @@ using ExemploExplorando.Models;
 // p1.Idade = 33;
 // p1.Apresentar();
 
-Pessoa p1 = new Pessoa(nome:"Anderson", sobrenome:"Oliveira");
-Pessoa p2 = new Pessoa(nome:"Luiz", sobrenome:"Oliveira");
+// Pessoa p1 = new Pessoa(nome:"Anderson", sobrenome:"Oliveira");
+// Pessoa p2 = new Pessoa(nome:"Luiz", sobrenome:"Oliveira");
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
